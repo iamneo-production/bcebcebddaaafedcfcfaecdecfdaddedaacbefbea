537b676932c31b97c18444129b095318f07b011d
Iamneo <techadmin@iamneo.ai>
2023-09-16 05:25:52 +0000

    Backup Commit

--- Task3.sh
+++ Task3.sh
@@ -2,16 +2,18 @@
 # for Folder , the permissions are rw_r__r__
 # for File , the permissions are rwxr__r__
 
-echo "Moving to the /home folder"
-cd /home
+echo "Moving to the / folder"
+cd /
+echo "Changing the owner of home folder"
+chown coder home
 echo "Creating a new Folder"
-sudo mkdir newFolder1
+mkdir newFolder1
 echo "Changing the permissions for the users,group and others as per given in the Task"
-sudo chmod u=rw,g=r,o=r newFolder1
+chmod u=rw,g=r,o=r newFolder1
 echo "Permissions to the new folder changed accordingly"
 echo "Creating a new File"
-sudo touch newFile2.txt
+touch newFile2.txt
 echo "Changing the permissions for the users,group and others as per given in the Task"
-sudo chmod u=rwx,g=r,o=r newFile2.txt
+chmod u=rwx,g=r,o=r newFile2.txt
 echo "Permissions to the new file changed accordingly"
 
