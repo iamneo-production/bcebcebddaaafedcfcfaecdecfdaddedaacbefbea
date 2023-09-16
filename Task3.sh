bdd6ad51747c1985af8bc5a1ed961880399d1131
Iamneo <techadmin@iamneo.ai>
2023-09-16 05:15:52 +0000

    Backup Commit

--- Task3.sh
+++ Task3.sh
@@ -1,2 +1,10 @@
-#Task3
+#Task3 - we need to create a folder and file in home folder with the following configurations
+# for Folder , the permissions are rw_r__r__
+# for File , the permissions are rwxr__r__
 
+echo "Moving to the /home folder"
+cd /home
+echo "Creating a new Folder"
+mkdir newFolder1
+echo "Changing the permissions for the users,group and others as per given in the Task"
+chmod u=
