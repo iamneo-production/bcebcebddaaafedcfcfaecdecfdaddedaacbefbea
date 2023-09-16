2b52441e6b087049d3dd38f09fb2218fc735ff53
Iamneo <techadmin@iamneo.ai>
2023-09-16 05:00:52 +0000

    Backup Commit

--- Task2.sh
+++ Task2.sh
@@ -1,3 +1,6 @@
 #File creation and writing some text content into it using shell scripting
 
-touch 
+echo "Creating a new File"
+touch newFile1.txt 
+read -p "Enter the text to put into the file" textContent 
+echo "$textContent" > newFile1.txt
