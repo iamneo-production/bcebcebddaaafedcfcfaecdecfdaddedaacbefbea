c345564eda69baca515423465d6a8bb53d46f7c8
Iamneo <techadmin@iamneo.ai>
2023-09-16 05:05:52 +0000

    Backup Commit

---
+++ Task4.sh
@@ -0,0 +1,6 @@
+#getting date,month,year from user and printing it in the DD-MM-YYYY format
+
+read -p "Enter the date of your birthday : " date
+read -p "Enter the month of your birthday : " month
+read -p "Enter the year of your birthday : " year
+echo "$date-$month-$year"
---
+++ Task5.sh
@@ -0,0 +1,4 @@
+#Getting two numbers from user and adding them and printing the output accordingly
+
+read -p "Enter the 1st number : " num1
+read -p "Enter the 2nd number : " num2
