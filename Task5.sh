b8bb6fd237f48fd9a26d7125a9436e2241f97a1b
Iamneo <techadmin@iamneo.ai>
2023-09-16 05:10:52 +0000

    Backup Commit

--- Task5.sh
+++ Task5.sh
@@ -2,3 +2,9 @@
 
 read -p "Enter the 1st number : " num1
 read -p "Enter the 2nd number : " num2
+sumOfThem=$((num1+num2))
+if [ $sumOfThem -gt 70 ]; then
+    echo "Pass"
+else 
+    echo "Fail"
+fi
